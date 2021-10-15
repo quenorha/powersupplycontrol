7b96e92d50cc45b7127c599ebc4e08751cfa478d
quenorha <51406726+quenorha@users.noreply.github.com>
2021-10-15 16:01:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 git clone https://github.com/sourceperl/pyModbusTCP.git
 cd pyModbusTCP
-sudo python setup.py install
-pip install pyModbusTCP
+python setup.py install
 chmod +x /root/powersupplycontrol/powersupplycontrol.py
 cp /root/powersupplycontrol/powersupplycontrol.service /etc/systemd/system/
 systemctl enable powersupplycontrol.service
