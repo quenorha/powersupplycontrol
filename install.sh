f839d22a9a418bab66d96d17f6b48e24c1a5859c
quenorha <51406726+quenorha@users.noreply.github.com>
2021-10-15 15:51:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install python3-pip
-pip install pymodbus
+apt-get -y install python-pip
+pip install pyModbusTCP
 chmod +x /root/powersupplycontrol/powersupplycontrol.py
 cp /root/powersupplycontrol/powersupplycontrol.service /etc/systemd/system/
 systemctl enable powersupplycontrol.service
