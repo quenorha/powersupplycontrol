cd25ed040e38e6c4109d5853fd6ed3607298e1fd
quenorha <51406726+quenorha@users.noreply.github.com>
2021-10-15 15:20:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git clone git://github.com/bashwork/pymodbus.git
 cd pymodbus
 python setup.py install
-chmod +x $PWD/powersupplycontrol/powersupplycontrol.py
-cp $PWD/powersupplycontrol/powersupplycontrol.service /etc/systemd/system/
+chmod +x /root/powersupplycontrol/powersupplycontrol.py
+cp /root/powersupplycontrol/powersupplycontrol.service /etc/systemd/system/
 systemctl enable powersupplycontrol.service
 systemctl start powersupplycontrol.service
