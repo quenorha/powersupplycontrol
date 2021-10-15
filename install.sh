005a870b614151c70b5b4b81cc24f49944f72581
quenorha <51406726+quenorha@users.noreply.github.com>
2021-10-15 15:59:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install python-pip
+git clone https://github.com/sourceperl/pyModbusTCP.git
+cd pyModbusTCP
+sudo python setup.py install
 pip install pyModbusTCP
 chmod +x /root/powersupplycontrol/powersupplycontrol.py
 cp /root/powersupplycontrol/powersupplycontrol.service /etc/systemd/system/
